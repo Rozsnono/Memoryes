403338db749061e2f9033745c985eb4ac93d3fcb
rozsnono <rozsnono@gmail.com>
2026-05-07 13:45:16 +0200

    cors

--- src/lib/cors.ts
+++ src/lib/cors.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 export function corsResponse(response: NextResponse, request?: Request) {
     const reqOrigin = request?.headers.get("origin") || "*";
 
-    response.headers.set("Access-Control-Allow-Origin", reqOrigin);
+    response.headers.set("Access-Control-Allow-Origin", "*");
     response.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     response.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
     response.headers.set("Access-Control-Max-Age", "86400");
